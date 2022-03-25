4261d14b5490977308714c64e49f3acebc8b3e74
Derek <bdai9798@gmail.com>
2022-03-25 12:22:14 +0100

    2.11 The Phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 import Persons from "./components/Persons";
 
 const App = (props) => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [unfilteredpersons, setUnfilteredpersons] = useState(persons);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+
+  const effectHook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setUnfilteredpersons(response.data);
+    });
+  };
+  useEffect(effectHook, []);
   const addPerson = (event) => {
     event.preventDefault();
     if (persons.find((person) => person.name === newName))
